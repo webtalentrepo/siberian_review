9770a7cf13b25c8fec93b789fa83e9926e3bb9f5
polaris610 <polaris.tiger803@yandex.com>
2016-09-12 16:44:58 +0300

    a

--- app/local/modules/Feedback/resources/db/data/feedback.php
+++ app/local/modules/Feedback/resources/db/data/feedback.php
@@ -53,72 +53,71 @@ $acl_resource
 	->setData($data)
 	->insertOnce(array("code"));
 
-// # Copy assets
-// $previewer_varpath = Core_Model_Directory::getBasePathTo("/app/local/modules/Feedback/resources/var/apps");
-// $varpath = Core_Model_Directory::getBasePathTo("/var/apps/browser");
+# Copy assets
+$previewer_varpath = Core_Model_Directory::getBasePathTo("/app/local/modules/Feedback/resources/var/apps");
+$varpath = Core_Model_Directory::getBasePathTo("/var/apps/browser");
 
-// if (file_exists($previewer_varpath) && file_exists($varpath)) {
-	// exec("cp -R {$previewer_varpath}/* {$varpath}/");
-	// exec("chmod -R 777 {$varpath}/");
-// }
+if (file_exists($previewer_varpath) && file_exists($varpath)) {
+	exec("cp -R {$previewer_varpath}/* {$varpath}/");
+	exec("chmod -R 777 {$varpath}/");
+}
 
-// $varpath1 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios/www");
-// if (file_exists($previewer_varpath) && file_exists($varpath1)) {
-	// exec("cp -R {$previewer_varpath}/* {$varpath1}/");
-	// exec("chmod -R 777 {$varpath1}/");
-// }
+$varpath1 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios/www");
+if (file_exists($previewer_varpath) && file_exists($varpath1)) {
+	exec("cp -R {$previewer_varpath}/* {$varpath1}/");
+	exec("chmod -R 777 {$varpath1}/");
+}
 
-// $varpath2 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/android/assets/www");
-// if (file_exists($previewer_varpath) && file_exists($varpath2)) {
-	// exec("cp -R {$previewer_varpath}/* {$varpath2}/");
-	// exec("chmod -R 777 {$varpath2}/");
-// }
+$varpath2 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/android/assets/www");
+if (file_exists($previewer_varpath) && file_exists($varpath2)) {
+	exec("cp -R {$previewer_varpath}/* {$varpath2}/");
+	exec("chmod -R 777 {$varpath2}/");
+}
 
-// $varpath3 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios-noads/www");
-// if (file_exists($previewer_varpath) && file_exists($varpath3)) {
-	// exec("cp -R {$previewer_varpath}/* {$varpath3}/");
-	// exec("chmod -R 777 {$varpath3}/");
-// }
+$varpath3 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios-noads/www");
+if (file_exists($previewer_varpath) && file_exists($varpath3)) {
+	exec("cp -R {$previewer_varpath}/* {$varpath3}/");
+	exec("chmod -R 777 {$varpath3}/");
+}
 
-// $oldStr = '<script src="js/controllers/radio.js"></script>';
-// $oldStr1 = '<script src="js/factory/radio.js"></script>';
-// $replaceStr = '<script src="js/controllers/radio.js"></script>
-		// <script src="js/controllers/feedback.js"></script>';
-// $replaceStr1 = '<script src="js/factory/radio.js"></script>
-		// <script src="js/factory/feedback.js"></script>';
+$indexHtml = Core_Model_Directory::getBasePathTo("/var/apps/browser/index.html");
+$str = file_get_contents($indexHtml);
+$oldStr = '<script src="js/controllers/radio.js"></script>';
+$oldStr1 = '<script src="js/factory/radio.js"></script>';
+$replaceStr = '<script src="js/controllers/radio.js"></script>
+<script src="js/controllers/feedback.js"></script>';
+$replaceStr1 = '<script src="js/factory/radio.js"></script>
+<script src="js/factory/feedback.js"></script>';
+if (strpos($str, "feedback") !== false) {
+} else {
+	$str = str_replace($oldStr, $replaceStr, $str);
+	$str = str_replace($oldStr1, $replaceStr1, $str);
+	file_put_contents($indexHtml, $str);
+}
 
-// $indexHtml = Core_Model_Directory::getBasePathTo("/var/apps/browser/index.html");
-// $str = file_get_contents($indexHtml);
-// if (strpos($str, "feedback") !== false) {
-// } else {
-	// $str = str_replace($oldStr, $replaceStr, $str);
-	// $str = str_replace($oldStr1, $replaceStr1, $str);
-	// file_put_contents($indexHtml, $str);
-// }
+$indexHtml1 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios/www/index.html");
+$str1 = file_get_contents($indexHtml1);
+if (strpos($str1, "feedback") !== false) {
+} else {
+	$str1 = str_replace($oldStr, $replaceStr, $str1);
+	$str1 = str_replace($oldStr1, $replaceStr1, $str1);
+	file_put_contents($indexHtml1, $str1);
+}
 
-// $indexHtml1 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios/www/index.html");
-// $str1 = file_get_contents($indexHtml1);
-// if (strpos($str1, "feedback") !== false) {
-// } else {
-	// $str1 = str_replace($oldStr, $replaceStr, $str1);
-	// $str1 = str_replace($oldStr1, $replaceStr1, $str1);
-	// file_put_contents($indexHtml1, $str1);
-// }
+$indexHtml2 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/android/assets/www/index.html");
+$str2 = file_get_contents($indexHtml2);
+if (strpos($str2, "feedback") !== false) {
+} else {
+	$str2 = str_replace($oldStr, $replaceStr, $str2);
+	$str2 = str_replace($oldStr1, $replaceStr1, $str2);
+	file_put_contents($indexHtml2, $str2);
+}
 
-// $indexHtml3 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios-noads/www/index.html");
-// $str3 = file_get_contents($indexHtml3);
-// if (strpos($str3, "feedback") !== false) {
-// } else {
-	// $str3 = str_replace($oldStr, $replaceStr, $str3);
-	// $str3 = str_replace($oldStr1, $replaceStr1, $str3);
-	// file_put_contents($indexHtml3, $str3);
-// }
-
-// $indexHtml2 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/android/assets/www/index.html");
-// $str2 = file_get_contents($indexHtml2);
-// if (strpos($str2, "feedback") !== false) {
-// } else {
-	// $str2 = str_replace($oldStr, $replaceStr, $str2);
-	// $str2 = str_replace($oldStr1, $replaceStr1, $str2);
-	// file_put_contents($indexHtml2, $str2);
-// }
+$indexHtml3 = Core_Model_Directory::getBasePathTo("/var/apps/ionic/ios-noads/www/index.html");
+$str3 = file_get_contents($indexHtml3);
+if (strpos($str3, "feedback") !== false) {
+} else {
+	$str3 = str_replace($oldStr, $replaceStr, $str3);
+	$str3 = str_replace($oldStr1, $replaceStr1, $str3);
+	file_put_contents($indexHtml3, $str3);
+}
--- var/apps/browser/prod.js
+++ var/apps/browser/prod.js
@@ -13484,6 +13484,91 @@ App.config(function($stateProvider, HomepageLayoutProvider) {
     $scope.loadContent();
 
 });
+;App.config(function ($stateProvider) {
+
+	$stateProvider.state('feedback-view', {
+		cache: false,
+		url: BASE_PATH + "/feedback/mobile_view/index/value_id/:value_id",
+		controller: 'FeedbackWriteController',
+		templateUrl: "templates/feedback/l1/view.html"
+	});
+
+}).controller('FeedbackWriteController', function ($rootScope, $scope, $stateParams, $translate, Dialog, Feedback) {
+
+	$scope.$on("connectionStateChange", function (event, args) {
+		if (args.isOnline == true) {
+			$scope.loadContent();
+		}
+	});
+
+	$scope.is_loading = true;
+	$scope.value_id = Feedback.value_id = $stateParams.value_id;
+	$scope.feedbackData = {};
+	$scope.feedbackData.user_email = '';
+	$scope.feedbackData.feedback_content = '';
+	$scope.feedbackData = {};
+
+	$scope.page_title = '';
+	$scope.customer_id = '';
+	$scope.loadContent = function () {
+		$scope.is_loading = true;
+		$scope.feedbackData = {};
+		$scope.feedbackData.user_email = '';
+		$scope.feedbackData.feedback_content = '';
+		$scope.feedbackData = {};
+		$scope.page_title = '';
+		$scope.customer_id = '';
+		$scope.value_id = Feedback.value_id = $stateParams.value_id;
+		Feedback.findAll().success(function (data) {
+			$scope.page_title = data.page_title;
+			$scope.customer_id = data.customer_id;
+			$scope.feedbackData.user_email = data.current_user;
+			$scope.feedbackData.feedback_content = data.feedback_content;
+		}).error(function () {
+
+		}).finally(function () {
+			$scope.is_loading = false;
+			$scope.closeFeedbackModal();
+		});
+
+	};
+
+	$scope.closeFeedbackModal = function() {
+		// Feedback.modal.hide();
+	};
+
+	$scope.post = function () {
+		if ($scope.feedbackData.user_email === null || $scope.feedbackData.user_email === '' || !$scope.feedbackData.user_email) {
+			Dialog.alert($translate.instant("Error"), "Please insert user email address", $translate.instant("OK"));
+			return;
+		}
+		$scope.feedbackData = {
+			'customer_id': $scope.customer_id,
+			'value_id': $scope.value_id,
+			'feedback_content': $scope.feedbackData.feedback_content,
+			'user_email': $scope.feedbackData.user_email
+		};
+		$scope.is_loading = true;
+
+		Feedback.post($scope.feedbackData).success(function (data) {
+			$scope.feedbackData = {};
+			if (data.success) {
+				Dialog.alert("", data.message, $translate.instant("OK"));
+				$scope.loadContent();
+				// $scope.closeFeedbackModal();
+			}
+		}).error(function (data) {
+			if (data && angular.isDefined(data.message)) {
+				Dialog.alert($translate.instant("Error"), data.message, $translate.instant("OK"));
+			}
+		}).finally(function () {
+			$scope.is_loading = false;
+		});
+	};
+
+	$scope.loadContent();
+
+});
 ;App.config(function($stateProvider) {
 
     $stateProvider.state('video-list', {
@@ -16455,6 +16540,36 @@ App.factory('MusicTrack', function ($http, $rootScope, Url) {
     return factory;
 });
 
+;App.factory('Feedback', function($http, $rootScope, Url) {
+
+	var factory = {};
+
+	factory.value_id = null;
+
+	factory.findAll = function() {
+
+		if(!this.value_id) return;
+
+		return $http({
+			method: 'GET',
+			url: Url.get("feedback/mobile_view/find", {value_id: this.value_id}),
+			cache: false,
+			responseType:'json'
+		});
+	};
+
+	factory.post = function (form) {
+
+		if (!this.value_id) return;
+
+		var url = Url.get("feedback/mobile_view/post", {value_id: this.value_id});
+		var data = {form: form};
+
+		return $http.post(url, data);
+	};
+
+	return factory;
+});
 ;App.factory('Video', function($rootScope, $http, $q, Url, Youtube) {
 
     var factory = {};
