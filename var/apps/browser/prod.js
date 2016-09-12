38e33930e3900435fb93b20a3067f49ee0c29143
polaris610 <polaris.tiger803@yandex.com>
2016-09-12 19:59:52 +0300

    feedback module changed

--- var/apps/browser/prod.js
+++ var/apps/browser/prod.js
@@ -6581,6 +6581,71 @@ $scope.getItineraryLink = function(point1,point2) {
     };
 });
 
+;App.directive('sbFeedbackRating', ['$timeout', function ($timeout) {
+	return {
+		restrict: 'EA',
+		require: ['rating', 'ngModel'],
+		scope: {
+			readonly: '=?'
+		},
+		template: '<ul class="rating" ng-keydown="onKeydown($event)">' + '<li ng-repeat="r in range track by $index" ng-click="rate($index + 1)"><i class="icon" ng-class="value === undefined ? (r === 1 ? \'ion-ios-star\' : (r === 2 ? \'ion-ios-star-half\' : \'ion-ios-star-outline\')) : (value > $index ? (value < $index+1 ? \'ion-ios-star-half\' : \'ion-ios-star\') : \'ion-ios-star-outline\')"></i></li>' + '</ul>',
+		replace: true,
+		link: function (scope, element, attrs, ctrls) {
+			var ngModelCtrl, ratingCtrl;
+			ratingCtrl = ctrls[0];
+			ngModelCtrl = ctrls[1];
+			if (ngModelCtrl) {
+				$timeout(function () {
+					return ratingCtrl.init(ngModelCtrl);
+				})
+			}
+		},
+		controller: function ($scope, $attrs, ratingConfig) {
+			var ngModelCtrl;
+			ngModelCtrl = {
+				$setViewValue: angular.noop
+			};
+			this.init = function (ngModelCtrl_) {
+				var max;
+				ngModelCtrl = ngModelCtrl_;
+				ngModelCtrl.$render = this.render;
+				max = angular.isDefined($attrs.max) ? $scope.$parent.$eval($attrs.max) : ratingConfig.max;
+				return $scope.range = this.buildTemplateObjects(ngModelCtrl.$modelValue, max);
+			};
+			this.buildTemplateObjects = function (stateValue, max) {
+				var j, states = [];
+				for (j = 0; j < max; j++) {
+					if (stateValue > j && stateValue < j + 1) {
+						states[j] = 2;
+					} else if (stateValue > j) {
+						states[j] = 1;
+					} else {
+						states[j] = 0;
+					}
+				}
+				return states;
+			};
+			$scope.rate = function (value) {
+				if (!$scope.readonly && value >= 0 && value <= $scope.range.length) {
+					ngModelCtrl.$setViewValue(value);
+					return ngModelCtrl.$render();
+				}
+			};
+			$scope.onKeydown = function (evt) {
+				if (/(37|38|39|40)/.test(evt.which)) {
+					evt.preventDefault();
+					evt.stopPropagation();
+					return $scope.rate($scope.value + (evt.which === 38 || evt.which === 39 ? {1: -1} : void 0));
+				}
+			};
+			this.render = function () {
+				return $scope.value = ngModelCtrl.$viewValue;
+			};
+			return this;
+		}
+	};
+}]);
+
 ;App.service('Application', function($http, $interval, $ionicPopup, $rootScope, $timeout, $translate, $window, Dialog, Url) {
 
     var service = {};
@@ -13491,88 +13556,91 @@ App.config(function($stateProvider, HomepageLayoutProvider) {
 		controller: 'FeedbackWriteController',
 		templateUrl: 'templates/feedback/l1/view.html'
 	});
-}).controller('FeedbackWriteController', function ($rootScope, $ionicModal, $window, $scope, $stateParams, $translate, Customer, Dialog, Feedback, AUTH_EVENTS) {
-	$scope.$on("connectionStateChange", function (event, args) {
-		if (args.isOnline == true && $scope.is_logged_in) {
-			$scope.loadContent();
-		}
-	});
-	$scope.$on(AUTH_EVENTS.loginSuccess, function () {
-		$scope.is_logged_in = true;
-		$scope.init();
-		$scope.loadContent();
-	});
-	$scope.$on(AUTH_EVENTS.logoutSuccess, function () {
-		$scope.is_logged_in = false;
-		$scope.login();
-	});
-	
-	$scope.is_logged_in = Customer.isLoggedIn();
-	$scope.login = function () {
-		$ionicModal.fromTemplateUrl('templates/customer/account/l1/login.html', {
-			scope: $scope,
-			animation: 'slide-in-up'
-		}).then(function (modal) {
-			Customer.modal = modal;
-			Customer.modal.show();
+}).controller('FeedbackWriteController', [
+	'$rootScope', '$ionicModal', '$window', '$scope', '$stateParams', '$translate', 'Customer', 'Dialog', 'Feedback', 'AUTH_EVENTS',
+	function ($rootScope, $ionicModal, $window, $scope, $stateParams, $translate, Customer, Dialog, Feedback, AUTH_EVENTS) {
+		$scope.$on("connectionStateChange", function (event, args) {
+			if (args.isOnline == true && $scope.is_logged_in) {
+				$scope.loadContent();
+			}
 		});
-	};
-	
-	$scope.init = function () {
-		$scope.is_loading = false;
-		$scope.value_id = Feedback.value_id = $stateParams.value_id;
-		$scope.feedbackData = {};
-		$scope.feedbackData.feedback_content = '';
-		$scope.page_title = '';
-		$scope.customer_id = '';
-	};
-	$scope.loadContent = function () {
-		$scope.is_loading = true;
-		$scope.feedbackData = {};
-		$scope.feedbackData.feedback_content = '';
-		$scope.page_title = '';
-		$scope.customer_id = '';
-		$scope.value_id = Feedback.value_id = $stateParams.value_id;
-		Feedback.findAll().success(function (data) {
-			$scope.page_title = data.page_title;
-			$scope.customer_id = data.customer_id;
-			$scope.feedbackData.feedback_content = data.feedback_content;
-		}).error(function () {
-		}).finally(function () {
-			$scope.is_loading = false;
+		$scope.$on(AUTH_EVENTS.loginSuccess, function () {
+			$scope.is_logged_in = true;
+			$scope.init();
+			$scope.loadContent();
+		});
+		$scope.$on(AUTH_EVENTS.logoutSuccess, function () {
+			$scope.is_logged_in = false;
+			$scope.login();
 		});
 		
-	};
-	$scope.post = function () {
-		$scope.feedbackData = {
-			'customer_id': $scope.customer_id,
-			'value_id': $scope.value_id,
-			'feedback_content': $scope.feedbackData.feedback_content
+		$scope.is_logged_in = Customer.isLoggedIn();
+		$scope.login = function () {
+			$ionicModal.fromTemplateUrl('templates/customer/account/l1/login.html', {
+				scope: $scope,
+				animation: 'slide-in-up'
+			}).then(function (modal) {
+				Customer.modal = modal;
+				Customer.modal.show();
+			});
 		};
-		$scope.is_loading = true;
-		Feedback.post($scope.feedbackData).success(function (data) {
-			$scope.feedbackData = {};
-			if (data.success) {
-				Dialog.alert('', data.message, $translate.instant('OK'));
-				$scope.loadContent();
-				// $scope.closeFeedbackModal();
-			}
-		}).error(function (data) {
-			if (data && angular.isDefined(data.message)) {
-				Dialog.alert($translate.instant('Error'), data.message, $translate.instant('OK'));
-			}
-		}).finally(function () {
+		
+		$scope.init = function () {
 			$scope.is_loading = false;
-		});
-	};
-	if (!$scope.is_logged_in) {
-		$scope.login();
-	} else {
-		$scope.is_logged_in = true;
-		$scope.init();
-		$scope.loadContent();
+			$scope.value_id = Feedback.value_id = $stateParams.value_id;
+			$scope.feedbackData = {};
+			$scope.feedbackData.feedback_content = '';
+			$scope.page_title = '';
+			$scope.customer_id = '';
+		};
+		$scope.loadContent = function () {
+			$scope.is_loading = true;
+			$scope.feedbackData = {};
+			$scope.feedbackData.feedback_content = '';
+			$scope.page_title = '';
+			$scope.customer_id = '';
+			$scope.value_id = Feedback.value_id = $stateParams.value_id;
+			Feedback.findAll().success(function (data) {
+				$scope.page_title = data.page_title;
+				$scope.customer_id = data.customer_id;
+				$scope.feedbackData.feedback_content = data.feedback_content;
+			}).error(function () {
+			}).finally(function () {
+				$scope.is_loading = false;
+			});
+			
+		};
+		$scope.post = function () {
+			$scope.feedbackData = {
+				'customer_id': $scope.customer_id,
+				'value_id': $scope.value_id,
+				'feedback_content': $scope.feedbackData.feedback_content
+			};
+			$scope.is_loading = true;
+			Feedback.post($scope.feedbackData).success(function (data) {
+				$scope.feedbackData = {};
+				if (data.success) {
+					Dialog.alert('', data.message, $translate.instant('OK'));
+					$scope.loadContent();
+					// $scope.closeFeedbackModal();
+				}
+			}).error(function (data) {
+				if (data && angular.isDefined(data.message)) {
+					Dialog.alert($translate.instant('Error'), data.message, $translate.instant('OK'));
+				}
+			}).finally(function () {
+				$scope.is_loading = false;
+			});
+		};
+		if (!$scope.is_logged_in) {
+			$scope.login();
+		} else {
+			$scope.is_logged_in = true;
+			$scope.init();
+			$scope.loadContent();
+		}
 	}
-});
+]);
 ;App.config(function($stateProvider) {
 
     $stateProvider.state('video-list', {
@@ -16544,36 +16612,26 @@ App.factory('MusicTrack', function ($http, $rootScope, Url) {
     return factory;
 });
 
-;App.factory('Feedback', function($http, $rootScope, Url) {
-
+;App.factory('Feedback', ['$http', '$rootScope', 'Url', function ($http, $rootScope, Url) {
 	var factory = {};
-
 	factory.value_id = null;
-
-	factory.findAll = function() {
-
-		if(!this.value_id) return;
-
+	factory.findAll = function () {
+		if (!this.value_id) return;
 		return $http({
 			method: 'GET',
 			url: Url.get("feedback/mobile_view/find", {value_id: this.value_id}),
 			cache: false,
-			responseType:'json'
+			responseType: 'json'
 		});
 	};
-
 	factory.post = function (form) {
-
 		if (!this.value_id) return;
-
 		var url = Url.get("feedback/mobile_view/post", {value_id: this.value_id});
 		var data = {form: form};
-
 		return $http.post(url, data);
 	};
-
 	return factory;
-});
+}]);
 ;App.factory('Video', function($rootScope, $http, $q, Url, Youtube) {
 
     var factory = {};
@@ -17834,6 +17892,10 @@ App.filter('translate', function ($translate) {
    clearSocialGaming: "clear-cache-socialgaming",
    clearDiscount: "clear-cache-discount"
 });
+;App.constant('FEEDBACK_CONFIG', {
+	max: 5
+});
+
 ;App.constant("PUSH_EVENTS", {
     notificationReceived: "push-notification-received",
     unreadPushs: "push-get-unreaded",
--- var/apps/ionic/android/assets/www/js/factory/feedback.js
+++ var/apps/ionic/android/assets/www/js/factory/feedback.js
@@ -1,30 +1,20 @@
-App.factory('Feedback', function($http, $rootScope, Url) {
-
+App.factory('Feedback', ['$http', '$rootScope', 'Url', function ($http, $rootScope, Url) {
 	var factory = {};
-
 	factory.value_id = null;
-
-	factory.findAll = function() {
-
-		if(!this.value_id) return;
-
+	factory.findAll = function () {
+		if (!this.value_id) return;
 		return $http({
 			method: 'GET',
 			url: Url.get("feedback/mobile_view/find", {value_id: this.value_id}),
 			cache: false,
-			responseType:'json'
+			responseType: 'json'
 		});
 	};
-
 	factory.post = function (form) {
-
 		if (!this.value_id) return;
-
 		var url = Url.get("feedback/mobile_view/post", {value_id: this.value_id});
 		var data = {form: form};
-
 		return $http.post(url, data);
 	};
-
 	return factory;
-});
+}]);
